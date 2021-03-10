80084a1848358e4796669bce7e68108aeeade964
Willian Ferreira <williannfferreira@gmail.com>
2021-03-09 22:15:02 -0300

    chore: initial commit (add files, add recipes endpoint)

---
+++ app/Http/Controllers/RecipeController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RecipeController extends Controller
+{
+    public function index(Request $request)
+    {
+        $ingredients = $request->i;
+        $client = new \GuzzleHttp\Client();
+        $res = $client->request('GET', env('RECIPEPUPPY_URL') . '?i=' . $ingredients);
+
+        $recipes = json_decode($res->getBody(), true);
+        return response()->json([
+            'keywords' => explode(',', $ingredients),
+            'recipes' => array_map(function ($recipe) use ($client) {
+                $newRecipe = [];
+                $newRecipe['title'] = $recipe['title'];
+                $newRecipe['ingredients'] = array_map('trim', explode(',', $recipe['ingredients']));
+                $newRecipe['link'] = $recipe['href'];
+                $resGif = $client->request('GET', 'https://api.giphy.com/v1/gifs/search?api_key=83B453UHWtOcJMdTWKEgIcn8Vwv1QA9k&q=' . $newRecipe['title'] . '&limit=25&offset=0&rating=g&lang=en');
+                $gifs = json_decode($resGif->getBody(), true);
+                $newRecipe['gif'] = $gifs['data'][0]['url'];
+                return $newRecipe;
+            }, $recipes['results'])
+        ]);
+    }
+}
